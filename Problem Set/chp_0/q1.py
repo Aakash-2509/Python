a7d2ea147ebd3d0684f4dcdeb99cf58ea037ce6a
Aakash <aakash.s.jagtap2001@gmail.com>
2024-09-05 12:08:23 +0530

    Python first Chapter with Problem Set

---
+++ Problem Set/chp_0/q1.py
@@ -0,0 +1,31 @@
+# Q1
+
+# print(
+#     """Twinkle, twinkle, little star, how I wonder what you are. Up above the world so high,
+# like a diamond in the sky. Twinkle, twinkle, little star, how I wonder what you are.
+# When the blazing sun is set, and the grass with dew is wet. Then you show your little
+# light, twinkle, twinkle all the night. Twinkle, twinkle little star, how I wonder what you
+# are.
+# Then the traveler in the dark thanks you for your tiny spark. How could he see where to
+# go if you did not twinkle so? Twinkle, twinkle little star, how I wonder what you are.
+# As your bright and tiny spark lights the traveler in the dark, though I know not what you
+# are, twinkle, twinkle, little star. Twinkle, twinkle, little star, how I wonder what you are."""
+# )
+
+# Q2
+
+# import pyttsx3
+# a= pyttsx3.init()
+# a.say("Hi Aakash, I know you can do it!!")
+# a.runAndWait()
+
+# Q3
+
+# import os 
+
+# dictory_path = '/'
+
+# contents = os.listdir(dictory_path)
+
+# for item in contents:
+#     print(item)
