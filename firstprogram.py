3d8b2743435576ea80afd1512d3e99e810ea1710
Aakash <aakash.s.jagtap2001@gmail.com>
2024-09-05 11:30:34 +0530

    Python first program

---
+++ firstprogram.py
@@ -0,0 +1,109 @@
+# name="Aakash"
+# age=22
+# age1=22.00
+# is_adult=True
+# abc =[1,2,['Aakash', 'Rahul']]
+# tuple=('abc','xyz','pqr',1,2,3)
+# a='1'
+# b='2'
+
+# print(name)
+# print(age)
+# print(is_adult)
+# print(abc)
+# print(tuple[2])
+# print(a+b)
+# print(int(a)+int(b))
+
+# print(type(name))
+# print(type(age))
+# print(type(age1))
+# print(type(is_adult))
+# print(type(abc))
+# print(type(tuple))
+# print("The type of tuple is ",type(tuple))
+# print(len(tuple))
+
+
+# name = input("Enter your name: ")
+# print("Name is ",name)
+
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+# print("Addition of two numbers is: " + str(int(x) + int(y)))
+
+
+# apple ='''Hello, Aakash How are you? Had Dinner?'''
+# for character in apple:
+#     print(character)
+    
+# name ='Aakash, Harry'
+# print(name[0:6])
+# print(len(name))
+# print(name[:4])
+# print(name[:]) #full string will be printed
+# print(name[0:-3]) #from end remove 3 char
+# print(name[-3:-1]) #print from -3 to -1 chars
+# print(name[::-1])
+# print(name[1:6:2]) #print from 1 to 6 with step (aah)
+
+
+# a1 ="aakash"
+# a="Another String"
+# print(a.lower()) 
+# print(a1.upper())
+
+# b = ",,..//AAKASH!!?"
+# print(b.rstrip("?"))
+# print(b.strip("?!/.,"))
+# print(b.replace("AAKASH","Ram"))
+
+# c = "!!!   Aakash   !!!"
+# print(c.lstrip("!"))
+# print(c.rstrip(" "))
+
+# print(3==2)
+# print(3!=2)
+
+# print(2>3 or 2<1)
+# print(2>3 and 2>1)
+# print(not 2>3)
+
+# age =22
+# if age>=25:
+#     print("You can drinks")
+# elif 21<=age<=24:
+#     print("You can have mild drinks")
+# else :
+#     print("You are a minor, you can't drink")
+
+
+# first_number = input("Enter first number : ")
+# opreator = input("Enter opreator (+,-,/,*,%) : ")
+# second_number = input("Enter second number : ")
+
+# if opreator == '+':
+#     print(int(first_number) + int(second_number))
+# elif opreator =='-':
+#     print(int(first_number)- int(second_number))
+# elif opreator=='*':
+#     print(int(first_number)*int(second_number))
+# elif opreator == '/':
+#     print(int(first_number)/int(second_number))
+# else :
+#     print(int(first_number)%int(second_number))
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i = i+1
+    
+i =1
+while i<=5:
+    print(i *"*")
+    i =i+1
+
+i =5
+while i>=0:
+    print(i *"*")
+    i =i-1
